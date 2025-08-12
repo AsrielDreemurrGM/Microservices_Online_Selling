b1f57c7f3c0886dd54a3f6d4df26020e4685f275
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-08-12 14:46:39 -0300

    Fixed ConfigServer YML And Updated ProductService
    - Added "application-version: 1.0" To Product-Service And Sales-Service YMLs;
    - Added "spring-cloud-starter-bootstrap" Dependency To ProductService POM;
    - Added Javadoc To ProductServiceApplication Class With Author And Date;
    - Changed "spring.application.name" To "product-service" In ProductService "application.properties";
    - Removed Duplicated "spring:" Indentation In All ConfigServer ".yml" Files;
    - Removed Unused "host" Property From Client-Service YML;

--- ProductService/src/main/java/br/com/eaugusto/onlineselling/ProductServiceApplication.java
+++ ProductService/src/main/java/br/com/eaugusto/onlineselling/ProductServiceApplication.java
@@ -3,6 +3,10 @@ package br.com.eaugusto.onlineselling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Eduardo Augusto (github.com/AsrielDreemurrGM/)
+ * @since Aug 09, 2025
+ */
 @SpringBootApplication
 public class ProductServiceApplication {
 
